9bea32b439eec74522ef3a35fb283121daef0d28
joonhyoung-kim <atar222@naver.com>
2022-06-07 20:02:10 +0900

    주문비교 진행중

---
+++ backend/src/main/resources/static/pp/XUPP1010.js
@@ -0,0 +1,22 @@
+var menuId = 'XUPP1010';
+var widget = momWidget;
+var that = undefined;
+var VIEW= {
+	initParam		: undefined, 
+	  
+	init: function() {	
+		that = this;	
+		that.event();
+	},
+	event: function(e) {
+	
+	}
+
+	
+	
+};
+
+$(document).ready(function(event){	
+	momWidget.init(1, menuId, VIEW);	
+	VIEW.init();
+});
