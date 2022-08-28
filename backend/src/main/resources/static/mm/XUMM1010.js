2e1758bd2a8614cb0fdffe577399abac88544889
atar2 <atar2@DESKTOP-G6BGM8L>
2022-08-28 14:54:14 +0900

    직적구매요청서 등록버튼 진행중

--- backend/src/main/resources/static/mm/XUMM1010.js
+++ backend/src/main/resources/static/mm/XUMM1010.js
@@ -18,9 +18,24 @@ var VIEW= {
 		
 
 	},
+	 createCallBack: function(index,your,action,btnId,param,result,data) {
+		if(index ==1 && btnId =='createBtn'){
+			let checkItem = widget.getCheckedRowItems(widget.grid[0]);
+		    $('#prNoDP2').val(checkItem[0]['prNo']);
+			/*var gridItem = [{langCd2:'KR',langNm:'한글',msgNm:''},{langCd2:'EN',langNm:'영어',msgNm:''}];
+			AUIGrid.setGridData(widget.grid[1], gridItem);*/
+				
+		}
+	},
 	 copyCallBack: function(index,your,action,btnId,param,result,data) {
 		if(index ==0 && btnId =='copyBtn'){	
 		    $('#prNoDP1').val('');
+			/*var gridItem = [{langCd2:'KR',langNm:'한글',msgNm:''},{langCd2:'EN',langNm:'영어',msgNm:''}];
+			AUIGrid.setGridData(widget.grid[1], gridItem);*/
+				
+		}
+		else if(index ==1 && btnId =='copyBtn'){	
+		    $('#prNoDP2').val('');
 			/*var gridItem = [{langCd2:'KR',langNm:'한글',msgNm:''},{langCd2:'EN',langNm:'영어',msgNm:''}];
 			AUIGrid.setGridData(widget.grid[1], gridItem);*/
 				
