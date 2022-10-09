70df6af0e3d8789bb1eab9a4205964d5a0a03da9
joonhyoung-kim <atar222@naver.com>
2022-10-09 16:40:28 +0900

    dd

--- backend/src/main/resources/static/mm/XUMM2100_1.js
+++ backend/src/main/resources/static/mm/XUMM2100_1.js
@@ -11,72 +11,68 @@ var VIEW= {
 	event: function(e) {
 	
 	},
-	editCallInit: function(index,your,action,btnId,param,result) {
-		if(index ==0 && btnId =='editBtn'){	
-			 let checkedItem = widget.getCheckedRowItems(widget.grid[index]);		
-              VIEW.partnerCd = checkedItem[0]['vendorCd'];
-		}
-	 
-
-	},
-	copyCallInit: function(index,your,action,btnId,param,result) {
-		if(index ==0 && btnId =='copyBtn'){	
-			 let checkedItem = widget.getCheckedRowItems(widget.grid[index]);		
-              VIEW.partnerCd = checkedItem[0]['vendorCd'];
-		}
-	 
-
-	},
-	savePopCallInit: function(index,your,action,btnId,param,result) {
-	     if(index ==0 && btnId =='saveBtnDP'){		    			
-			 param[0].partnerCd =VIEW.partnerCd;
-			 result.param =  param;
-		    
-	     }
-	
-	  
-	},
-	createCallInit: function(index,your,action,btnId,param,result) { //등록버튼 팝업띄우기 전에 호출되는 함수 
-		if(index ==0 && btnId =='createBtn1'){			
-			$('#poReferenceTypeDP1').val('10');
-	        
-					
-		}
-
-	},
     searchCallInit: function(index,your,action,btnId,param,result,event) { //조회액션 실행 전에 호출되는 함수 
         if(index==1){
-			
-			  					
+				let checkItem = widget.getCheckedRowItems(widget.grid[0]);
+			    result.param = {receiveNo:checkItem[0].receiveNo};	
 		} 
 	
 
 		
 	},
-    cellClickCallBack: function(index,rowIndex,target,e) {				
-		if(index==100 && target=='vendorNm'){
-			$('#vendorNmDP1').val(e.item.partnerCd+'('+e.item.partnerNm+')');
-            VIEW.partnerCd = e.item.partnerCd;
+	 copyCallBack: function(index,your,action,btnId,param,result,data) {  //등록버튼 팝업띄우고나서 호출되는 함수 
+		if(index ==0 && btnId =='copyBtn'){
+			$('#receiveNoDP1').val('');		
 		}
-		else if(index==0){
-			widget.findBtnClicked(1, {poNo:e.item['poNo']}, true, 'CELLCLICK',menuId,VIEW);
+	},
+  cellClickCallBack: function(index,rowIndex,target,e) {				
+		 if(index==0){
+			widget.findBtnClicked(1, {receiveNo:e.item['receiveNo']}, true, 'CELLCLICK',menuId,VIEW);
 		}
+		
 			
 	},
 	customCallInit: function(index,your,action,btnId,param,result) {
-		if(index == 0 ){
-			if(action =='D' || btnId == 'customBtn1-2'){
-			   /* for(var i=0,max=param.length; i<max;i++){
-					 if(param[i].poUserNo == undefined || param[i].poUserNo == ''){
-							result.msg = '발주담당자를 지정하지 않은 데이터가 있습니다!';
-							result.result = 'WARN';
-							return;
-					 }		    
-			    }*/
-	
+			var checkItem = widget.getCheckedRowItems(widget.grid[0]);
+		if(index == 1){
+			if(btnId == 'customGridPopBtn2-1'){ // 커스텀 버튼 실행시 1 삭제(D) 2 TMP삽입(C) 3 프로시저실행(P) actionType 으로 시점 제어가능  		
+			if(checkItem.length==0){
+				//$('#' +'gridPop-'+btnId).momModal('hide');
+				result.msg = '상단에서 입고서 선택필수!';
+				result.result = 'WARN';
+				return;
+				//widget.messageBox({type: 'warning', width: '400', height: '145', html: '상단에서 납품서 선택필수!'});				 
+			    //return;
+			}
+	           
+					   param['vendorCd'] = checkItem[0]['vendorCd'];
+					   param['departureLocationCd'] = checkItem[0]['departureLocationCd'];
+					   param['iqcFlag'] = checkItem[0]['iqcFlag'];
+					   param['currencyCd'] = checkItem[0]['currencyCd'];			
+					   $('#vendorCdSP21').val(checkItem[0]['vendorCd']);
+					   $('#departureLocationCdSP21').val(checkItem[0]['departureLocationCd']);
+					   $('#iqcFlagSP21').val(checkItem[0]['iqcFlag']);
+					   $('#currencyCdSP21').val(checkItem[0]['currencyCd']);
+					   //result.param = param;		 
+			 
+		   }
+		   else if(btnId == 'customBtn2-2'){
+			 for(var i=0,max=param.length; i<max;i++){
+					 param[i]['inoutNo'] = checkItem[0]['departureNo'];
+					 
+			 }
 		   }
 		}
+	
+	    else if(index == 20){			    
+			    if(action='C'&& btnId == 'customBtn21-1'){ 
+				    for(var i=0,max=param.length; i<max;i++){
+					 param[i]['inoutNo'] = checkItem[0]['receiveNo'];
+					 
+			        }
+		}
 		
+	    }
 	}
 	
 };
@@ -85,6 +81,6 @@ $(document).ready(function(event){
 	momSetup.init();
 	momWidget.init(1, menuId, VIEW,'GRID');	
 	momWidget.init(2, menuId, VIEW,'GRID');	
-	momWidget.init(11, 'XUSM8060', VIEW,'DG');
+	momWidget.init(2, 'XUSM8100', VIEW,'DG');
 	VIEW.init();
 });
