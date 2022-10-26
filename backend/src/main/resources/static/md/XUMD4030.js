06868ae8c81d4a3b7e31a6103c98d205c343b13a
joonhyoung-kim <atar222@naver.com>
2022-10-26 17:25:49 +0900

    팝업및 화면 리사이징및 XML 오류 수정 ,작업장관리 까지 확인, XUMD3020 영업단가관리 테스트확인해야함

--- backend/src/main/resources/static/md/XUMD4030.js
+++ backend/src/main/resources/static/md/XUMD4030.js
@@ -5,19 +5,15 @@ var VIEW= {
 	init: function() {	
 	},
 	savePopCallInit: function(index,your,action,btnId,param,result) {
-	     if(index ==0 && btnId =='saveBtnDP'){		           
-					    var items = param
+	     if(index ==0 && (btnId =='createBtn' || btnId =='editBtn'|| btnId =='copyBtn')){		           
+					    let items = param
 					    var startTime = items[0]['startTime'].split(':');
 					    var endTime   = items[0]['endTime'].split(':');
 					    var startTimeToSec = Number(startTime[0])*3600 + Number(startTime[1])*60;
 					    var endTimeToSec = Number(endTime[0])*3600 + Number(endTime[1])*60;
 					    var restTime = (endTimeToSec - startTimeToSec )/60;
-			/*	        for(var i=0;i<gridItems.length;i++){
-					         gridItems[i]['msgType']     = $("#msgType2").val();
-					         gridItems[i]['msgCd']       = $("#msgCd2").val();					         
-					        
-					    }*/
-					    items[0]['restTime'] = restTime;
+
+					     items[0]['restTime'] = restTime;
 			             result.param = items;
         
 	    }
