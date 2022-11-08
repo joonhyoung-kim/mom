fa64f26406560d933e33a7e47a8fe9262811521c
joonhyoung-kim <atar222@naver.com>
2022-11-08 14:15:27 +0900

    clear git cache

--- backend/target/classes/static/content/AUIGrid/AUIGridLicense.js
+++
@@ -1,9 +0,0 @@
-/*!
-* AUIGrid 3.0.11 License
-* License Type :  Enterprise Dev License
-* Authorized Domain(or IP) : 211.192.46.217
-* Expiration Date :  2023-03-31
-* www.auisoft.net
-*/
-/* eslint-disable */
-AUIGridLicense = "eyJjdCI6ImxKM1ZSTkxsR3diN2JkWnA5NDRSajZTRFh1OWZMalRDR2pNcmVzYVwvY09mVXlkTVlVRUE4MnpFa3p3TjNoTElIb0hNdEJSZk41Ykw3WDM5eHBvTE1xQ21NeHdLSlYxQnp6U2xWbzZaWWM0U2F0cm1LYmp2NmZLd2hqdUFUM25GRCIsIml2IjoiODljY2VlZDNkODAxN2VjYTk4Zjk3MzIzNzkyMjA5OGMiLCJzIjoiZDk5YzFmNjA0ODEwNjRkMyJ9";
