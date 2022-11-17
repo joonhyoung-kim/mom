5bc9fd87d259f405374b53b7782e4d66f87ab2ba
cslee <lcs@e-calces.com>
2022-11-17 17:25:31 +0900

    신규화면 추가-(XUPP1050)

---
+++ backend/src/main/resources/static/pp/XUPP1050.js
@@ -0,0 +1,21 @@
+var menuId = 'XUPP1050';
+var widget = momWidget;
+var that = undefined;
+var VIEW= {
+	initParam		: undefined, 
+	  
+	init: function() {	
+		that = this;	
+		that.event();
+	},
+	event: function(e) {
+	
+	}
+
+};
+
+$(document).ready(function(event){	
+	momSetup.init();
+	momWidget.init(1, menuId, VIEW);	
+	VIEW.init();
+});
