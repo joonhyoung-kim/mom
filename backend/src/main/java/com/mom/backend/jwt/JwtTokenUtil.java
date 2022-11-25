d455df4ba6902ea813e0494ca59dcfb41ed9a47c
cslee <lcs@e-calces.com>
2022-11-25 17:00:02 +0900

    로그인 토큰 발급시간 수정

--- backend/src/main/java/com/mom/backend/jwt/JwtTokenUtil.java
+++ backend/src/main/java/com/mom/backend/jwt/JwtTokenUtil.java
@@ -15,7 +15,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
-    public static final long JWT_TOKEN_VALIDITY = 60;//
+    public static final long JWT_TOKEN_VALIDITY = 3600*10;
     @Value("${jwt.secret}")
     private String secret;
 
