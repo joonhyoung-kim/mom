01d9299e0c6de811985793ba841fa83cb3ec9d6d
cslee <lcs@e-calces.com>
2022-11-30 17:17:19 +0900

    XML 구문 수정

--- backend/src/main/resources/static/md/XUMD4030.js
+++ backend/src/main/resources/static/md/XUMD4030.js
@@ -5,36 +5,28 @@ var VIEW= {
 	init: function() {	
 	},
 	savePopCallInit: function(index,your,action,btnId,param,result) {
-	     if(index ==0 && (btnId =='createBtn' || btnId =='editBtn'|| btnId =='copyBtn')){		           
-					    let items = param
-					    var startTime = items[0]['startTime'].split(':');
-					    var endTime   = items[0]['endTime'].split(':');
-					    var startTimeToSec = Number(startTime[0])*3600 + Number(startTime[1])*60;
-					    var endTimeToSec = Number(endTime[0])*3600 + Number(endTime[1])*60;
-					    if(startTimeToSec>endTimeToSec){
-						items[0]['restTime'] = (86400 + (endTimeToSec - startTimeToSec ))/60
-					    }
-					    else if(startTimeToSec == endTimeToSec){
-						items[0]['restTime'] = 1440;
-			          
-					    }
-					    else{
-						items[0]['restTime'] = (endTimeToSec - startTimeToSec )/60;
-			          
-					    }
-					     result.param = items;
-        
-        
-        
-					
+	    if(index ==0 && (btnId =='createBtn' || btnId =='editBtn'|| btnId =='copyBtn')){		           
+		    let items = param
+			var startTime = items[0]['startTime'].split(':');
+			var endTime   = items[0]['endTime'].split(':');
+			var startTimeToSec = Number(startTime[0])*3600 + Number(startTime[1])*60;
+			var endTimeToSec = Number(endTime[0])*3600 + Number(endTime[1])*60;
+			if(startTimeToSec>endTimeToSec){
+			    items[0]['restTime'] = (86400 + (endTimeToSec - startTimeToSec ))/60
+			}
+			else if(startTimeToSec == endTimeToSec){
+			    items[0]['restTime'] = 1440;
+			}
+			else{
+			    items[0]['restTime'] = (endTimeToSec - startTimeToSec )/60;
+			}
+			result.param = items;
 	    }
-
-	}
-	
+    }
 };
 
 $(document).ready(function(event){	
-	momSetup.init();
+    momSetup.init();
 	momWidget.init(1, menuId, VIEW);
 	VIEW.init();
 });
