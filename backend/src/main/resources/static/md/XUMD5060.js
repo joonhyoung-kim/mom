e6fd672767dd889cec78c63cf5563bcbe4f7bd11
cslee <lcs@e-calces.com>
2022-12-12 13:23:32 +0900

    신규화면 추가

---
+++ backend/src/main/resources/static/md/XUMD5060.js
@@ -0,0 +1,19 @@
+var menuId = 'XUMD5060';
+var widget = momWidget;
+var that = undefined;
+var VIEW= {
+	initParam		: undefined, 
+	partnerCd       : undefined,
+	init: function() {	
+		that = this;	
+		that.event();
+	},
+	event: function(e) {
+	},
+};
+
+$(document).ready(function(event){	
+	momSetup.init();
+	momWidget.init(1, menuId, VIEW,'GRID');	
+	VIEW.init();
+});
